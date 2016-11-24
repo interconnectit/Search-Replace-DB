ae59cdb68669fb9cb8daabd56997bf7d343a6e6c
zanzix <zans.lancs@gmail.com>
2016-11-24 11:04:39 +0000

    put css and js into their own files & use CDN version of jquery

--- templates/html.php
+++ templates/html.php
@@ -1,20 +1,17 @@
 <!DOCTYPE html>
 <html class="<?php echo implode(' ', $classes); ?>">
-
 <head>
     <title>interconnect/it : search replace db</title>
-
-    <meta charset="utf-8"/>
-    <LINK href="assets/css/style.css" rel="stylesheet" type="text/css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="assets/js/scripts.js"></script>
-    <script>var h = document.getElementsByTagName('html')[0];
-        h.className = h.className.replace('no-js', 'js');</script>
-
+    <meta charset="utf-8">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script>
+        var h = document.getElementsByTagName('html')[0];
+        h.className = h.className.replace('no-js', 'js');
+    </script>
 </head>
 <body>
-
 <?php $this->$body(); ?>
-
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="assets/js/scripts.js"></script>
 </body>
 </html>
