cda010f656d1d94d7e6a75afff0bd30b0b0d4a34
Jack Reynard <42768516+jack-interconnectit@users.noreply.github.com>
2020-03-26 09:59:13 +0000

    remove modal code

--- index.php
+++ index.php
@@ -2267,30 +2267,6 @@ window.console = window.console || {
                         $('[type="submit"].db-required').removeAttr('disabled');
                     t.running = false;
                     $('.stop-button').remove();
-
-                    var $button = $('[type="submit"]'),
-                        submit = $button.attr('name');
-                        console.log(submit)
-                    if (submit.match(/liverun/)) {
-                        // Get the modal
-                        var modal = document.getElementById("deleteModal");
-
-                        // Get the <span> element that closes the modal
-                        var span = document.getElementsByClassName("close")[0];
-
-                        // When the user clicks on <span> (x), close the modal
-                        span.onclick = function() {
-                            modal.style.display = "none";
-                        }
-
-                        // When the user clicks anywhere outside of the modal, close it
-                        window.onclick = function(event) {
-                            if (event.target == modal) {
-                                modal.style.display = "none";
-                            }
-                        }
-                        modal.style.display = "block";
-                    }
                 },
 
                 recursive_fetch_json: function (data, i) {
